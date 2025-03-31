4a6d171fa224c525c1c59a8e84b7ab01063e1177
SHUVAM KUMAR SINGH <118374131+MrIncursio03@users.noreply.github.com>
2025-03-31 21:27:03 +0530

    Create Move All Zeroes to End.py

---
+++ Move All Zeroes to End.py
@@ -0,0 +1,8 @@
+class Solution:
+	def pushZerosToEnd(self,arr):
+    	# code here
+    	count=0
+    	for i in range (len(arr)):
+    	    if arr[i] !=0:
+    	        arr[i],arr[count]=arr[count],arr[i]
+    	        count = count+1
